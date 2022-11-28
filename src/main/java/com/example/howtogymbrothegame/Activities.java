aa0440e3ab50928f5b9b1115a5de0937bb5eec49
Petervesthem <pehem22@student.sdu.dk>
2022-11-28 22:04:23 +0100

    Activities switch metode

--- src/main/java/com/example/howtogymbrothegame/Activities.java
+++ src/main/java/com/example/howtogymbrothegame/Activities.java
@@ -1,13 +1,36 @@
 package com.example.howtogymbrothegame;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Activities {
+
+    private Parent root;
+    private Stage stage;
+    private Scene scene;
+
     public void infoSmoking(){
         System.out.println("langvarigt rygning i mellem 1-4 cigaretter dagligt vil signifikant øge risikoen for at dø af hjertesygdomme.");
         System.out.println("Konsekvenserne ved for højt tobaksindtag kan føre iskemi, som nedsætter blodtilgang til dele af hjertemusklen.");
         System.out.println("Og kan derudover medføre til alle former for cancer");
         System.out.println("Vil du virkelig udsætte dig selv for det?");
+    }
+
+    @FXML
+    public void switchtoWeekDays(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Weekday.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public void smokingAsk(){
