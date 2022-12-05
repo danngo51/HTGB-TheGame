485ac6a1cc22941966f826152693b167ac9f3319
danngo51 <danngo51@gmail.com>
2022-12-05 23:31:09 +0100

    new class and overview fxml file made

---
+++ src/main/java/com/example/howtogymbrothegame/OverviewController.java
@@ -0,0 +1,69 @@
+package com.example.howtogymbrothegame;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+
+public class OverviewController {
+    //Attributes and methods for tab1
+    @FXML
+    private Tab stats;
+    @FXML
+    private Label heartOverview;
+    @FXML
+    private Label lungsOverview;
+    @FXML
+    private Label liverOverview;
+    @FXML
+    private Label pancreasOverview;
+
+    //Attributes and methods for tab2
+    @FXML
+    private Tab stats3mo;
+    @FXML
+    private Label heartOverview3mo;
+    @FXML
+    private Label lungsOverview3mo;
+    @FXML
+    private Label liverOverview3mo;
+    @FXML
+    private Label pancreasOverview3mo;
+
+    //Attributes and methods for tab3
+    @FXML
+    private Tab stats6mo;
+    @FXML
+    private Label heartOverview6mo;
+    @FXML
+    private Label lungsOverview6mo;
+    @FXML
+    private Label liverOverview6mo;
+    @FXML
+    private Label pancreasOverview6mo;
+
+    //Attributes and methods for tab4
+    @FXML
+    private Tab stats9mo;
+    @FXML
+    private Label heartOverview9mo;
+    @FXML
+    private Label lungsOverview9mo;
+    @FXML
+    private Label liverOverview9mo;
+    @FXML
+    private Label pancreasOverview9mo;
+
+    //Attributes and methods for tab5
+    @FXML
+    private Tab stats12mo;
+    @FXML
+    private Label heartOverview12mo;
+    @FXML
+    private Label lungsOverview12mo;
+    @FXML
+    private Label liverOverview12mo;
+    @FXML
+    private Label pancreasOverview12mo;
+
+
+}
