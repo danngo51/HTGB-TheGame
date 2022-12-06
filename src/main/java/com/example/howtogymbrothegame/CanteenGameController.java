b677ae240eb765daab2a96fe3372634c28883f9a
danngo51 <danngo51@gmail.com>
2022-12-06 18:11:55 +0100

    mini game updates

--- src/main/java/com/example/howtogymbrothegame/CanteenGameController.java
+++ src/main/java/com/example/howtogymbrothegame/CanteenGameController.java
@@ -47,53 +47,51 @@ public class CanteenGameController {
 
     private void tomato(MouseEvent event) {
         setItemScorePositiv();
-
+        tomato.setVisible(false);
     }
-
     private void cucumber(MouseEvent event) {
         setItemScorePositiv();
+        cucumber.setVisible(false);
     }
-
     private void steakMeat(MouseEvent event) {
         setItemScorePositiv();
+        steakMeat.setVisible(false);
     }
-
     private void watermelon(MouseEvent event) {
         setItemScorePositiv();
+        watermelon.setVisible(false);
     }
-
     private void carrot(MouseEvent event) {
         setItemScorePositiv();
+        carrot.setVisible(false);
     }
-
     private void chicken(MouseEvent event) {
         setItemScorePositiv();
+        chicken.setVisible(false);
     }
-
     private void bellpeper(MouseEvent event) {
         setItemScorePositiv();
+        bellpeper.setVisible(false);
     }
-
     private void rice(MouseEvent event) {
         setItemScoreNegativ();
+        rice.setVisible(false);
     }
-
     private void candy(MouseEvent event) {
         setItemScoreNegativ();
+        candy.setVisible(false);
     }
-
     private void soda(MouseEvent event) {
         setItemScoreNegativ();
+        soda.setVisible(false);
     }
-
     private void cake(MouseEvent event) {
         setItemScoreNegativ();
-    }
+        cake.setVisible(false);
 
     private void setItemScorePositiv() {
         this.itemScore = itemScore + 1;
     }
-
     private void setItemScoreNegativ() {
         this.itemScore = itemScore - 1;
     }
